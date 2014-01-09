6764dbc0d0dc1329b19dea2603b3fa0ae00e556f
saerem21 <nemessisdds>
2014-01-09 10:31:08 +0900

    Initial Commit

---
+++ Ex1.1.1_MVC_tableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ex1.1.1_MVC_tableView
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ex1.1.1_MVC_tableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex1.1.1_MVC_tableView
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
